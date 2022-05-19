028b014a2dfb6f7cf1e0db03db34037890519208
Jaemin Bae <jaebae@c5r5s6.42seoul.kr>
2022-05-19 21:44:50 +0900

    C07 ex02 commit

--- C07/ex02/Array.hpp
+++ C07/ex02/Array.hpp
@@ -1,27 +1,69 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-template <typenaem T>
+# include <exception>
+
+template <typename T>
 class Array
 {
 private:
-    T* _array;
+    T*  _array;
+    int _n;
 public:
     Array()
     : _array(nullptr)
     {}
-    Array(unsigned int n)
-    : _array(new T[n])
+
+    Array(int n)
+    : _array(new T[n]), _n(n)
     {}
+
+    ~Array()
+    {
+        delete [] _array;
+    }
+
     Array(const Array& dummy)
     {
-
+        *this = dummy;
     }
+
     Array& operator = (const Array& dummy)
     {
-        this->_array
+        _array = new T[dummy._n];
+        _n = dummy._n;
+        for (int i = 0; i < _n; i++)
+            _array[i] = dummy._array[i];
+        return (*this);
     }
 
+    class GradeTooHighException : public std::exception
+    {
+    public:
+        const char* what() const throw()
+        {
+            return ("invalid index");
+        }
+    };
+
+    T& operator [] (int idx)
+    {
+        if (_array == nullptr || idx >= _n || idx < 0)
+            throw GradeTooHighException();
+        return (_array[idx]);
+    }
+
+    T operator [] (int idx) const
+    {
+        if (_array == nullptr || idx >= _n || idx < 0)
+            throw GradeTooHighException();
+        return (_array[idx]);
+    }
+
+    int& size() const 
+    {
+        return (_n);
+    }
 };
 
 #endif
--- C07/ex02/main.cpp
+++ C07/ex02/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <Array.hpp>
+#include "Array.hpp"
 
 #define MAX_VAL 750
 int main(int, char**)
@@ -48,6 +48,6 @@ int main(int, char**)
     {
         numbers[i] = rand();
     }
-    delete [] mirror;//
+    delete [] mirror;
     return 0;
 }
